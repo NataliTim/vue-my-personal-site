ee3900b421abd061cdf825475c21d9384a427b44
NataliTim <tymofeieva.n@gmail.com>
2020-07-06 15:45:12 -0400

    add bootstrap and uninstall bootstrap-vue

--- vuecliproject/src/App.vue
+++ vuecliproject/src/App.vue
@@ -1,9 +1,6 @@
 <template>
     <div id="app">
         <div class="wrapper">
-        <div class="lang_wrapper">
-            <LocaleSwitcher/>
-            </div>
             <Main/>
         </div>
     </div>
@@ -12,23 +9,11 @@
 <script>
 
     import Main from './components/Main.vue'
-    import LocaleSwitcher from './components/LocaleSwitcher.vue'
-    import Vue from 'vue'
-    import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
-
-    // Install BootstrapVue
-    Vue.use(BootstrapVue)
-    // Optionally install the BootstrapVue icon components plugin
-    Vue.use(IconsPlugin)
-
-    import 'bootstrap/dist/css/bootstrap.css'
-    import 'bootstrap-vue/dist/bootstrap-vue.css'
 
     export default {
         name: 'App',
         components: {
-            Main,
-            LocaleSwitcher
+            Main
         }
     }
 </script>
---
+++ vuecliproject/src/components/Main.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+        <div class="container">
+            <div class="lang_wrapper">
+                <LocaleSwitcher/>
+            </div>
+            <p>{{ $t('main.title') }}</p>
+            <p>{{ $t('main.slogan') }}</p>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                With Bootstrap!
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import LocaleSwitcher from './LocaleSwitcher.vue'
+
+    export default {
+        data() {
+            return {
+                dismissSecs: 10,
+                dismissCountDown: 0,
+                showDismissibleAlert: false
+            }
+        },
+        components: {
+            LocaleSwitcher
+        },
+        methods: {
+            countDownChanged(dismissCountDown) {
+                this.dismissCountDown = dismissCountDown
+            },
+            showAlert() {
+                this.dismissCountDown = this.dismissSecs
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
